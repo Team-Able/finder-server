ebc8f8036c05391f4914e430350b2e06a5ddd41e
전민오 <jbj338033@gmail.com>
2024-08-15 10:13:04 +0900

    test: github test

---
+++ src/main/java/com/finder/domain/lostitem/controller/LostItemController.java
@@ -0,0 +1,12 @@
+package com.finder.domain.lostitem.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lost-items")
+@RequiredArgsConstructor
+public class LostItemController {
+
+}
---
+++ src/main/java/com/finder/domain/lostitem/domain/entity/LostItemEntity.java
@@ -0,0 +1,23 @@
+package com.finder.domain.lostitem.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@Entity
+@Table(name = "lost_items")
+public class LostItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
