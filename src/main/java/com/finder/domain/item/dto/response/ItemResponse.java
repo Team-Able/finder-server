1b4cfc8cc14fe7e6cb8f980c6c0797ec84babbfe
전민오 <jbj338033@gmail.com>
2024-09-28 20:29:50 +0900

    feat: why title and content are null

--- src/main/java/com/finder/domain/item/dto/response/ItemResponse.java
+++ src/main/java/com/finder/domain/item/dto/response/ItemResponse.java
@@ -3,13 +3,17 @@ package com.finder.domain.item.dto.response;
 import com.finder.domain.item.domain.entity.ItemEntity;
 import com.finder.domain.item.domain.enums.ItemStatus;
 
+import java.time.LocalDateTime;
+
 public record ItemResponse(
         Long id,
         String title,
         String content,
-        ItemStatus status
+        ItemStatus status,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) {
     public static ItemResponse of(ItemEntity item) {
-        return new ItemResponse(item.getId(), item.getTitle(), item.getContent(), item.getStatus());
+        return new ItemResponse(item.getId(), item.getTitle(), item.getContent(), item.getStatus(), item.getCreatedAt(), item.getUpdatedAt());
     }
 }
--- src/main/java/com/finder/domain/item/service/impl/ItemServiceImpl.java
+++ src/main/java/com/finder/domain/item/service/impl/ItemServiceImpl.java
@@ -33,6 +33,8 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemResponse createItem(ItemCreateRequest request) {
+        System.out.println(request.title());
+
         ItemEntity item = itemRepository.save(ItemEntity.builder()
                 .title(request.title())
                 .content(request.content())
