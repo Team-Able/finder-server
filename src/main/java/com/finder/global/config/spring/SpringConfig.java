2b96a67d22c0e13fc6fa3ce1481f42cbc5689660
전민오 <jbj338033@gmail.com>
2024-09-28 20:32:16 +0900

    feat: object mapper

--- src/main/java/com/finder/global/config/spring/SpringConfig.java
+++ src/main/java/com/finder/global/config/spring/SpringConfig.java
@@ -1,6 +1,8 @@
 package com.finder.global.config.spring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 }
