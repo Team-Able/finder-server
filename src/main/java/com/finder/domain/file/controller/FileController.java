202e53c520e5603c51f56fd29f11a3958dc0dba8
전민오 <jbj338033@gmail.com>
2024-10-12 20:23:14 +0900

    feat: make upload file returns filename

--- src/main/java/com/finder/domain/file/controller/FileController.java
+++ src/main/java/com/finder/domain/file/controller/FileController.java
@@ -1,10 +1,12 @@
 package com.finder.domain.file.controller;
 
 import com.finder.domain.file.service.FileService;
+import com.finder.global.common.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +22,7 @@ public class FileController {
 
     @Operation(summary = "파일 업로드")
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void upload(@RequestParam("file") MultipartFile file) {
-        fileService.uploadFile(file);
+    public ResponseEntity<BaseResponse<String>> upload(@RequestParam("file") MultipartFile file) {
+        return BaseResponse.of(fileService.uploadFile(file), 201, "파일 업로드 성공");
     }
 }
