012ce704e4ecf6f48a03047fb8cb3cb220bca915
Simseoh <seohoon0707@gmail.com>
2024-12-16 14:53:55 +0900

    fix: userSecession

--- src/main/java/com/finder/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/finder/domain/user/service/impl/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void secession() {
             itemRepository.deleteAllByAuthor(securityHolder.getPrincipal());
-            userRepository.deleteByEmail(securityHolder.getPrincipal().getEmail());
+            userRepository.deleteById(securityHolder.getPrincipal().getId());
     }
 
     @Override
